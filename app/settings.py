d11d3798e0d6fffce2fc29ddcbc197bc0d49b0cd
eduardoAlves <eduardo.santos.alves14@gamil.com>
2025-06-02 16:24:38 -0300

    Update ALLOWED_HOSTS in settings.py to include production URL

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-#%6@ew@e(uk!-_e+*-fv2yevy5m)s1%b$9x__wnlgwz4&u6d(n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://dadosfaker.onrender.com"]
 
 
 # Application definition
